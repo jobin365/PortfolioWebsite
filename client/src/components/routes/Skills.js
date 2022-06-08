eec1eccd804741a4df0c7f72a66bc9f753162232
Jobin John K <jobinjohnk5@gmail.com>
2022-06-08 21:53:33 +0530

    add html and css skills

--- client/src/components/routes/Skills.js
+++ client/src/components/routes/Skills.js
@@ -5,6 +5,8 @@ import MongoDBLogo from "../images/mongo-min.png";
 import ExpressLogo from "../images/express-min.png";
 import GitLogo from "../images/git-min.png";
 import JSLogo from "../images/js.png";
+import HTMLLogo from "../images/html.png";
+import CSSLogo from "../images/css.png";
 import { v4 as uuidv4 } from 'uuid';
 import "./Skills.css"
 export default function About() {
@@ -13,7 +15,9 @@ export default function About() {
     ["MongoDB",MongoDBLogo ],
     ["Express",ExpressLogo],
     ["Git",GitLogo],
-    ["JavaScript",JSLogo]
+    ["JavaScript",JSLogo],
+    ["CSS",CSSLogo],
+    ["HTML",HTMLLogo]
   ];
   return (
     <div className="skills">
