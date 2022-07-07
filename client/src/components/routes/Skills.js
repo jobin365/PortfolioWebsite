a6adf10f629350006b53e9b840ad3eaba65d9612
Jobin John K <jobinjohnk5@gmail.com>
2022-07-07 09:50:28 +0530

    updated skills

--- client/src/components/routes/Skills.js
+++ client/src/components/routes/Skills.js
@@ -1,23 +1,19 @@
 import React from "react";
 import LogoNName from "../LogoNName";
-import ReactLogo from "../images/react-min.png";
-import MongoDBLogo from "../images/mongo-min.png";
-import ExpressLogo from "../images/express-min.png";
 import GitLogo from "../images/git-min.png";
 import JSLogo from "../images/js.png";
-import HTMLLogo from "../images/html.png";
-import CSSLogo from "../images/css.png";
+import mernLogo from "../images/mern.png";
+import htmlNcssLogo from "../images/html&css.png"
+import sfLogo from "../images/salesforce.png";
 import { v4 as uuidv4 } from 'uuid';
 import "./Skills.css"
 export default function About() {
   const skills = [
-    ["React", ReactLogo ],
-    ["MongoDB",MongoDBLogo ],
-    ["Express",ExpressLogo],
+    ["Salesforce Marketing Cloud",sfLogo],
+    ["MERN Stack Development",mernLogo],
     ["Git",GitLogo],
     ["JavaScript",JSLogo],
-    ["CSS",CSSLogo],
-    ["HTML",HTMLLogo]
+    ["HTML & CSS",htmlNcssLogo]
   ];
   return (
     <div className="skills">
